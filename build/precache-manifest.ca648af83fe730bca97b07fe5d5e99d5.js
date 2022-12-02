dcaee7477bdc686cba4a9760d243e26285670435
SeldomseenSchweig <jamesschweig@gmail.com>
2022-12-02 07:46:14 -0800

    deployed, changed color on back of card

---
+++ build/precache-manifest.ca648af83fe730bca97b07fe5d5e99d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8907d53c607389f0a848",
+    "url": "/static/css/main.9bcb5cea.chunk.css"
+  },
+  {
+    "revision": "8907d53c607389f0a848",
+    "url": "/static/js/main.8907d53c.chunk.js"
+  },
+  {
+    "revision": "1879641eedd1047e981c",
+    "url": "/static/css/1.7e294131.chunk.css"
+  },
+  {
+    "revision": "1879641eedd1047e981c",
+    "url": "/static/js/1.1879641e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b69fcc491b3f3b192a0c3045454cc68",
+    "url": "/index.html"
+  }
+];
