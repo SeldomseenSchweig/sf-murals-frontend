333ae51db5ab4cf3302c10dd8a71f9d8e730dacd
SeldomseenSchweig <jamesschweig@gmail.com>
2023-08-13 16:25:08 -0400

    trying to see token

--- src/api.js
+++ src/api.js
@@ -25,7 +25,7 @@ class sfMuralsApi {
     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const url = `${BASE_URL}/${endpoint}`;
-    console.log("This shold be url: ",url, "tokenID: ",sfMuralsApi.token)
+    console.log("This shold be url: ",url, "tokenID: ",sfMuralsApi)
     const headers = { Authorization: `Bearer ${sfMuralsApi.token}` };
     const params = (method === "get")
       ? data
