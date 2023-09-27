5c3ff5d1e067f26f318ba76f05eb556610d02ec9
SeldomseenSchweig <jamesschweig@gmail.com>
2023-09-27 14:25:35 -0700

    changed css

--- src/Home.js
+++ src/Home.js
@@ -7,43 +7,11 @@ import CurrentUserContext from "./CurrentUserContext";
 import { Link } from "react-router-dom";
 import './Home.css';
 
-// Import images for background and decorative purposes
-import muralImage from './mural.jpg';
+
 
 function Home() {
   const user = useContext(CurrentUserContext);
 
-  // const styles = {
-  //   card: {
-  //     backgroundImage: `url(${muralImage})`,
-  //     backgroundSize: "cover",
-  //     color: "white",
-  //   },
-  //   title: {
-  //     backgroundColor: "rgba(0, 0, 0, 0.7)",
-  //     padding: "20px",
-  //     borderRadius: "10px",
-  //   },
-  //   header: {
-  //     fontSize: "3rem",
-  //     fontWeight: "bold",
-  //     marginBottom: "20px",
-  //   },
-  //   subTitle: {
-  //     fontSize: "1.5rem",
-  //     marginBottom: "30px",
-  //   },
-  //   buttonGroup: {
-  //     display: "flex",
-  //     justifyContent: "center",
-  //   },
-  //   button: {
-  //     margin: "0 10px",
-  //     padding: "10px 20px",
-  //     fontSize: "1.2rem",
-  //     fontWeight: "bold",
-  //   },
-  // };
 
   return (
     <section className="col-md-14">
